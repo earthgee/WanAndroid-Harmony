7acba8910dfcdfbd36200dd186ca863e54060640
earthgee <865603556@qq.com>
2024-04-21 23:24:11 +0800

    study

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,8 +1,11 @@
 import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
+import WindowUtils from '../common/WindowUtils'
 
 export default class EntryAbility extends UIAbility {
+  tag: string = "EntryAbility"
+
   onCreate(want, launchParam) {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
   }
@@ -11,9 +14,13 @@ export default class EntryAbility extends UIAbility {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
   }
 
-  onWindowStageCreate(windowStage: window.WindowStage) {
+  async onWindowStageCreate(windowStage: window.WindowStage) {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+
+    let windowClass = await windowStage.getMainWindow();
+    hilog.info(0x0000, this.tag, 'Succeeded in obtaining the main window. Data: ' + JSON.stringify(windowClass));
+    WindowUtils.init(windowClass);
 
     windowStage.loadContent('pages/Index', (err, data) => {
       if (err.code) {
